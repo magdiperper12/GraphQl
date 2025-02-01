1d4bc9e2dc4e439404b75de197d384c97ffd1d62
magdiperper12 <162820515+magdiperper12@users.noreply.github.com>
2025-02-01 11:49:19 +0200

    adding array of users

--- server.js
+++ server.js
@@ -5,14 +5,38 @@ const { buildSchema } = require('graphql');
 const app = express();
 const port = 3000;
 
+const users = [
+	{
+		id: '23',
+		name: ' magdi',
+		age: '25',
+	},
+	{
+		id: '2',
+		name: ' saleh',
+		age: '22',
+	},
+	{
+		id: '78',
+		name: ' fathi',
+		age: '58',
+	},
+];
 const schema = buildSchema(`
+	type User{
+	 id: String ,
+    name: String ,
+    age: String 
+}
   type Query {
-    hello: String 
+    hello: String ,
+   usersGet:[User]
   }
 `);
 
 const resolver = {
 	hello: () => 'hello world my name is magdi',
+	usersGet: () => users,
 };
 
 app.use(
